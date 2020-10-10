84a26792fd21992cb1bec1c42d8b36dba73d44b2
Mabat <mabat250@gmail.com>
2020-10-10 19:57:18 +0200

    added jQueryUI

--- dev/src/web/scripts/ls.js
+++ dev/src/web/scripts/ls.js
@@ -1,6 +1,14 @@
 function AddNote(){
-    var title = document.getElementsByName("title")[0].value;
-    var content = document.getElementsByName("con")[0].value;
+    const title = document.getElementsByName("title")[0].value;
+    const content = document.getElementsByName("con")[0].value;
 
-    localStorage.setItem()
+    const arr = [{"title":title, "content":content}];
+
+    const data =  JSON.parse(localStorage.getItem("notes"));
+
+    if(data!=null){
+        data.forEach(element => arr.push(element));
+    }
+    localStorage.setItem("notes", JSON.stringify(arr));
+    location.reload();
 }
