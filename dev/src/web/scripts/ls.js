33022ea2cb16d1c7c49360684e49162c9ca3180d
Mabat <mabat250@gmail.com>
2020-10-11 12:51:51 +0200

    added option to remove notes

--- dev/src/web/scripts/ls.js
+++ dev/src/web/scripts/ls.js
@@ -17,9 +17,42 @@ function AddNote(){
     }
     localStorage.setItem("notes", JSON.stringify(arr));
     ClearForm();
-    location.reload();
+    ReloadNotes();
 }
 function ClearForm(){
     document.getElementsByName("title")[0].value = "";
     document.getElementsByName("con")[0].value = "";
+}
+function ReloadNotes(){
+    var acc = document.getElementById("accordion");
+    acc.innerHTML = "";
+
+    const notes = JSON.parse(localStorage.getItem("notes"));
+    if(notes == null)return;
+    var i = 0;
+    notes.forEach(function(element){
+        var header = document.createElement("h3");
+        var title = document.createTextNode(element.title);
+
+        var btn = document.createElement("button");
+        btn.setAttribute("onclick", "RemoveNote("+JSON.stringify(i++)+")");
+        btn.innerHTML = "X";
+
+        header.appendChild(title);
+        header.appendChild(btn);
+        acc.appendChild(header);
+
+
+        var content = document.createElement("div");
+        var contentText = document.createTextNode(element.content);
+        content.appendChild(contentText);
+        acc.appendChild(content);
+    });
+    $( "#accordion" ).accordion("refresh");
+}
+function RemoveNote(id){
+    const data =  JSON.parse(localStorage.getItem("notes"));
+    data.splice(id,1);
+    localStorage.setItem("notes", JSON.stringify(data));
+    ReloadNotes();
 }
